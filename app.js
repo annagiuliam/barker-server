0377f0078f0be943c8a10d6b80cf25c279772c2b
Anna <anna.musso@cm4all.com>
2024-11-20 16:17:49 +0100

    feat: added basic db connection

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require("dotenv").config();
+const express = require('express');
+
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const app = express();
+const db = require('./connection');
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+// Connect to MySQL
+
+
+// Routes
+app.get('/accounts', (req, res) => {
+  db.query('SELECT * FROM accounts', (err, results) => {
+    if (err) {
+      console.error('Error executing query: ' + err.stack);
+      res.status(500).send('Error fetching users');
+      return;
+    }
+    res.json(results);
+  });
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
